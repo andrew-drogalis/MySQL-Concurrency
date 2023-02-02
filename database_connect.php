59331deeff82e594b425af21ae782afa946ad9da
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2023-02-01 19:52:16 -0500

    Initial commit

---
+++ database_connect.php
@@ -0,0 +1,5 @@
+<?php
+// Connects to Database Once Required in Public Directory File
+$con = mysqli_connect("hostname","username","password","database");
+
+?>
---
+++ src/concurrency_example.php
@@ -0,0 +1,80 @@
+<?php
+ 
+// Create Connection Outside Public Directory
+require_once '../database_connect.php';
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
+if (!isset($_POST['password'])) {
+	// Could not get the data that should have been sent.
+	exit('Please enter the access password.');
+}
+
+$result = mysqli_query($con, "SELECT * FROM accounts WHERE id = 1");
+$hashed_password = $result->fetch_object()->password;
+
+// Password Stored and Hashed upon Account Creation - See Example Below
+// password_hash($password, PASSWORD_BCRYPT);  
+
+if (password_verify($_POST['password'], $hashed_password)) {
+
+    // Lock Read & Write Access
+    mysqli_query($con, "LOCK TABLES Work_Order_Active WRITE;");
+
+    // This SQL statement selects ALL from the table 'Work Order'
+    $sql = "SELECT * FROM Work_Order_Active WHERE id=(SELECT max(id) FROM Work_Order_Active)";
+
+    // Check if there are results
+    if ($result = mysqli_query($con, $sql))
+    {
+        $row = $result->fetch_object();
+        // Increment Value 
+        $number_int = $row->work_order_number + 10;
+        $number_str = strval($number_int);
+
+        // Update Table with new Work Order Number
+        $sql_insert = $con->prepare("UPDATE Work_Order_Active SET work_order_number=? WHERE id=1");
+        $sql_insert -> bind_param("s",$number_str);
+        $sql_insert->execute();
+        $sql_insert->close();
+
+        // Create JSON to echo to Client
+        $resultArray = array();
+        $number_int = $row->work_order_number;
+        // Create Array of New Work Order Numbers
+        for ($x = 0; $x < 10; $x++) {
+            $number_int += 1;
+
+            // Add Leading 0 String to Work Order Number
+            if ($number_int < 10000) {
+                $number_str = "000" . strval($number_int);
+            } elseif (10000 <= $number_int && $number_int < 100000){
+                $number_str = "00" . strval($number_int);
+            } elseif (100000 <= $number_int && $number_int < 1000000){
+                $number_str = "0" . strval($number_int);
+            } else{
+                $number_str = strval($number_int);
+            }
+            $temp_array = ["work_order_number" => $number_str];
+            array_push($resultArray,$temp_array);
+        }
+
+        // Finally, encode the array to JSON and output the results
+        echo json_encode($resultArray);
+    }
+
+    // Unlock Read & Write 
+    mysqli_query($con, "UNLOCK TABLES;");
+
+} else {
+    // Incorrect username
+    echo 'Incorrect username and/or password!';
+}
+	
+// Close connections
+mysqli_close($con);
+
+?>
